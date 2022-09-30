1095369b7eeca87334177165e00f056e55e28681
Ehsan Davari <ehsanwatch11@gmail.com>
2022-10-01 01:21:48 +0330

    Upgrade

--- translator.py
+++ translator.py
@@ -1,12 +1,60 @@
-#Author : Mohammad Fallahzade
+"""Authores
+    Names
+        Ehsan Davari
+        Mohammad Fallahzade
+    GithubUsernames
+        iamehsandvr
+        mofallahzade
+    Version
+        1.2.0
+"""
 from translators import google
+
+""" Languages """
+
+
+class Languages:
+    def __init__(self) -> None:
+        self.strLanguages = {
+            "fa": {
+                "welcome": "متن خودرا وارد کنید :",
+                "f-enter": "زبان متن وارد شده را وارد کنید ({0}):",
+                "l-enter": "زبانی که میخواید متن به آن ترجمه شود را وارد کنید ({0}):",
+                "error": "زبان وارد شده در لیست زبان های وارد شده وجود نداره یا شما قبلا این زبان رو انتخاب کردید"
+            },
+            "en": {
+                "welcome": "Enter the text: ",
+                "f-enter": "Enter the text language({0}): ",
+                "l-enter": "Translate to({0}): ",
+                "error": "The language you want is not in the list of supported languages or this language has already been selected."
+            },
+        }
+
+
+""" Index Code """
+
+
 class translators:
-    def __init__(self):
-        self.text = input("Enter the text: ")
-        self.language = input("Enter the text language(en/fa): ")
-        self.translationLanguage = input("Translate to(en/fa): ")
+    def __init__(self, strLT="en") -> None:
+        self.strlstLanguages = ["en", "fa"]
+        self.strLanguagesString = "/".join(self.strlstLanguages)
+        self.text = input((Languages().strLanguages.get(strLT)).get("welcome"))
+        try:
+            self.language = input(
+                ((Languages().strLanguages.get(strLT)).get("f-enter").format(self.strLanguagesString)))
+            self.strlstLanguages.remove(self.language)
+            self.strLanguagesString = "/".join(self.strlstLanguages)
+            self.translationLanguage = input(
+                (Languages().strLanguages.get(strLT)).get("l-enter").format(self.strLanguagesString))
+            self.strlstLanguages.remove(self.translationLanguage)
+        except:
+            print((Languages().strLanguages.get(strLT)).get("error"))
+            translators().translate()
+
     def translate(self):
-        self.translatedText = google(self.text, self.language,self.translationLanguage)
+        self.translatedText = google(
+            self.text, self.language, self.translationLanguage)
         print(self.translatedText)
-trans = translators()
-trans.translate()
+
+
+translators(strLT="en").translate()
